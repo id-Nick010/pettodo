b95a7fef33e543ae07d4b49fa4c19bb06e5c708d
Daphne Agao <agaodaphne@gmail.com>
2024-08-12 01:03:42 +0800

    functional splashscreen

--- lib/features/app/splash_screen.dart
+++ lib/features/app/splash_screen.dart
@@ -6,7 +6,6 @@ import "package:flutter/services.dart";
 import "../user_auth/presentation/pages/log_in.dart";
 import "../user_auth/presentation/widgets/progress_bar.dart";
 
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -16,13 +15,24 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
-      late double percentage = 0.0;
-    
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
+      _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+      );
+          _animation = Tween<double>(begin: 0, end: 1).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _animationController.forward();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => LogInScreen()),
       );
@@ -33,6 +43,7 @@ class _SplashScreenState extends State<SplashScreen>
   void dispose() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: SystemUiOverlay.values);
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -41,26 +52,23 @@ class _SplashScreenState extends State<SplashScreen>
     return Scaffold(
       body: Container(
           width: double.infinity,
-          color: const Color(0xFFFFFAF0), // Set background color to whitish yellow
+          color:
+              const Color(0xFFFFFAF0), // Set background color to whitish yellow
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Image(
                 image: AssetImage('assets/logo-light.png'),
                 width: 296,
                 height: 297),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center, 
-              children: [
-                const SizedBox(height: 20), //space
-                Text('Make sure to finish your tasks for your pets!',
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(height: 20), //space
+              Text('Make sure to finish your tasks for your pets!',
                   style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0XFF2B2B2E)
-                  )
-                ),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0XFF2B2B2E))),
             ]),
             const SizedBox(height: 20), //space
-            ProgressBar(width: 200, height: 20, progress: percentage)
+            ProgressBar(width: 200, height: 20, progress: _animation.value)
           ])),
     );
   }
